70d52b086414a26be58be09378a6f442a103dc01
mubashir-cvr <ahnas>
2021-12-17 16:47:57 +0530

    Added

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,12 @@
 from django.urls import path,include
 from .views import *
 
+app_name='web'
+
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('about/',about,name='about'),
+    path('branch/',branch,name='branch'),
+    path('contact/',contact,name='contact'),
+    path('doctor/',doctor,name='doctor')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+
+def about(request):
+    return render(request,'aboutus.html')
+
+
+def branch(request):
+    return render(request,'branch.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def doctor(request):
+    return render(request,'doctor.html')
