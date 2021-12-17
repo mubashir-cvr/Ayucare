883ac0425b6994282b064837df3ead537697c6c1
mubashir-cvr <ahnas>
2021-12-17 11:59:57 +0530

    Static Files and Template

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(".toggle-button").click(function () {
+    $(".site-navigationn").css("display", "block");
+}); 
+
+$("#closse").click(function () {
+    $(".site-navigationn").css("display", "none");
+}); 
+
+
+
+
